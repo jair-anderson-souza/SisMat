cb95210a6f496d9df63e047548679bf0be2858d6
Anderson Souza <jair_anderson_bs@hotmail.com>
2017-11-16 01:45:10 -0300

    validando campos de identidade e número do documento

--- src/main/java/io/github/jass2125/mat/core/util/IdentidadeValidador.java
+++ src/main/java/io/github/jass2125/mat/core/util/IdentidadeValidador.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.github.jass2125.mat.core.util;
+
 
 import java.math.BigInteger;
 import javax.faces.application.FacesMessage;
@@ -13,9 +9,16 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 /**
  *
- * @author Anderson Souza
+ * @author <a href="mailto:jair_anderson_bs@hotmail.com">Anderson Souza</a>
+ * @author 16/11/2017 01:43:13
  */
 @FacesValidator("identidade.validator")
 public class IdentidadeValidador implements Validator {
