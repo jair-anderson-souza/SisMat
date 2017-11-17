6c4f15487553d54fe2161ffb2ebe3e7db1ac8804
Anderson Souza <jair_anderson_bs@hotmail.com>
2017-11-17 19:48:20 -0300

    retirando arquivos criados pela IDE

--- bin/docs/script.sql
+++
@@ -1,9 +0,0 @@
-create table alunx(
-    cpf varchar(15) primary key,
-    identidade varchar(30) not null unique,
-    nome varchar(40) not null,
-    numeroDocumentoMilitar varchar(30),
-    dtNasc date not null,
-    nacionalidade varchar(30) not null,
-    sexo varchar(30) not null
-);
